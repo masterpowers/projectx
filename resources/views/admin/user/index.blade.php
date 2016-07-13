9c52b53532b3d1e6183a5a64c5eda07d333becb1
Uriah Galang <masterpowers001@gmail.com>
2016-07-13 11:55:45 +0800

    fuck my computer

--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -1,4 +1,41 @@
-List of All Users:
+@extends('layouts.material')
+@section('content')
+<!-- YOUR MAIN PAGE CONTENT IS HERE!!! -->
+<!-- TO ADD PAGINATION IN CONTENT USE THIS -->
+{{-- @if($items->hasPages())
+    <div class="pagination-wrapper">
+        <div class="pagination-wrapper-inner">
+            {!! (new App\Pagination($))->render() !!}
+        </div>
+    </div>
+@endif --}}
+<table>
+    <thead>
+      <tr>
+          <th data-field="id">ID<th>
+          <th data-field="username">Username</th>
+          <th data-field="price">Item Price</th>
+      </tr>
+    </thead>
+
+    <tbody>
+      <tr>
+        <td>Alvin</td>
+        <td>Eclair</td>
+        <td>$0.87</td>
+      </tr>
+      <tr>
+        <td>Alan</td>
+        <td>Jellybean</td>
+        <td>$3.76</td>
+      </tr>
+      <tr>
+        <td>Jonathan</td>
+        <td>Lollipop</td>
+        <td>$7.00</td>
+      </tr>
+    </tbody>
+</table>
 <br>
 <hr>
 {{ $users }}
@@ -6,3 +43,4 @@ List of All Users:
 @foreach($users as $user)
 {{ $user->email }}
 @endforeach
+@endsection
